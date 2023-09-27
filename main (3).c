58ba2df149514a46c01013798e5478f2c8dea10e
23Rh <141180744+23Rh@users.noreply.github.com>
2023-09-27 17:34:37 -0300

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,24 @@
+// INSTRUÇÕES DE PRÉ PROCESSAMENTO
+#include <stdio.h>
+
+// DECLARAÇÃO DE VARIÁVEIS
+int lado = 10;
+
+int base = 7;
+int altura = 3;
+
+int area = 0;
+
+// ENTRADAS DE DADOS
+
+// FUNÇÃO PRINCIPAL
+int main() {
+
+  // PROCESSAMENTOS
+  area = base * altura;
+
+  // SAÍDAS DE DADOS
+  printf("A área de figura é: %d", area);
+
+  return 0;
+}
